6101da7b4f5b11d303b9acbcde582ae7154a28aa
voidking <voidking@qq.com>
2017-02-18 20:40:15 +0800

    finish svm mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,44 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+clf = SVC(kernel='rbf',C=10000.)
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'training time:',round(time() - t0, 3),'s'
+
+# print clf.score(features_test, labels_test)
+predictions = clf.predict(features_test)
+# print predictions[10]
+# print predictions[26]
+# print predictions[50]
+
+count = 0
+for result in predictions:
+    if result == 1:
+        count = count + 1
+print count
 
 #########################################################
 
+'''
+training time: 207.594 s
+0.984072810011
 
+training time: 0.124 s
+0.884527872582
+
+training time: 0.139 s
+0.616040955631
+
+training time: 0.127 s
+0.892491467577
+
+training time: 137.599 s
+877
+
+'''
