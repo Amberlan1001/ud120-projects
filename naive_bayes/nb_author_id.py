4d019e6dc291566c3c7506047963599645c5136d
voidking <voidking@qq.com>
2017-02-18 17:13:20 +0800

    finish naive bayes test about identity

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'training time:',round(time() - t0, 3),'s'
+
+print clf.score(features_test, labels_test)
 
 #########################################################
+
+'''
+no. of Chris training emails: 7936
+no. of Sara training emails: 7884
+training time: 1.517 s
+0.973265073948
+'''
 
 
